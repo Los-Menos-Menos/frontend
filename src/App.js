fabe742265dd2e4b90e4f30a7c394619585330cd
Sebastian <sebastian.sepulvedab@sansano.usm.cl>
2022-11-11 15:39:07 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import React, {Component} from 'react';
 
 import opciones from './opciones.js';
 
-import Residente from "./Componentes/residente/residente";
-import Gastoscomunes_residente from "./Componentes/residente/gastoscomunes_residente";
+
 import Navbar from "./Componentes/navbar";
 import Footer from './Componentes/footer';
+
+import Login from './Componentes/login';
+
 import AdminInicio from './Componentes/admin/admin_inicio';
 import AdminUsuarios from './Componentes/admin/admin_usuarios';
 import AdminReservas from './Componentes/admin/admin_reservas';
-import Login from './Componentes/login';
+
 import Conserje from './Componentes/conserje/conserje';
 import Libro from './Componentes/conserje/libro';
-import Anuncios from './Componentes/residente/anuncios';
+
+import Directiva from './Componentes/directiva/directiva';
 import Anuncios_Directiva from './Componentes/directiva/anuncios_directiva';
 import Nuevo_Anuncio from './Componentes/directiva/nuevo_anuncio';
+import InfoResidentes_Directiva from './Componentes/directiva/info_residentes';
+
+import Residente from "./Componentes/residente/residente";
+import Gastoscomunes_residente from "./Componentes/residente/gastoscomunes_residente";
+import Anuncios from './Componentes/residente/anuncios';
 import Reservas_Residente from './Componentes/residente/reservas_residente';
 import Reservas_Quincho from './Componentes/residente/reserva_quincho';
 import Reservas_Estacionamiento from './Componentes/residente/reserva_estacionamiento';
 import Reservas_SalaEventos from './Componentes/residente/reserva_salaeventos';
 import Reservas_Multicancha from './Componentes/residente/reserva_multicancha';
-import InfoResidentes_Directiva from './Componentes/directiva/info_residentes';
+
 
 class App extends Component {
     constructor(){
@@ -104,7 +112,7 @@ class App extends Component {
 			case "Directiva":
 				switch(this.state.page){
 					case "Directiva":
-						return <Anuncios_Directiva cambiarPagina={this.cambiarPagina}/>
+						return <Directiva cambiarPagina={this.cambiarPagina}/>
 					case "Nuevo_Anuncio":
 						return <Nuevo_Anuncio cambiarPagina={this.cambiarPagina}/>
 					default:

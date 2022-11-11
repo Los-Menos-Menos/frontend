1d0677651400db18b4582db321c37e01a9535c8a
Luis Corrales <luis.corrales@sansano.usm.cl>
2022-11-11 18:03:27 -0300

    Update: Tabla gestion usuarios Add: Form de Crear usuario

---
+++ src/Componentes/admin/data_usuarios.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      title: "Alfredo Llanos",
+      year: "2022/01/15",
+      morosidad: "$1000",
+      email: "alfreditoxgamermaestro@runin.cl",
+      numero: "123456789",
+      tipoUsuario:"Administrador",
+      //idUsuario: "1"
+    },
+    {
+      title: "Fernanda Cerda",
+      year: "2022/02/15",
+      morosidad: "$2000",
+      email: "nandito@nonogram.cl",
+      numero: "234567891",
+      tipoUsuario:"Directiva",
+      //idUsuario: "2"
+    },
+    {
+        title: "Luis Corrales",
+        year: "2022/03/15",
+        morosidad: "$1500",
+        email: "lxchodelagb@colocolo.cl",
+        numero: "345678912",
+        tipoUsuario:"Residente",
+        //idUsuario: "3"
+    },
+    {
+        title: "Sofia Mañana",
+        year: "2022/04/15",
+        morosidad: "$4000",
+        email: "luchitoesmuymaloelcolocolo@jajajaj.cl",
+        numero: "456789123",
+        tipoUsuario:"Conserje",
+        //idUsuario: "4"
+    },
+    {
+        title: "Sebastián Sepulveda",
+        year: "2022/05/15",
+        morosidad: "$5000",
+        email: "feñitateamo@amor.cl",
+        numero: "123456789",
+        tipoUsuario:"Residente",
+        //idUsuario: "5"
+    }
+];

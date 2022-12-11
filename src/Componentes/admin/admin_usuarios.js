7a242ae81a1909f584ad79f43e7d45460545dee2
nandink <fernanda.cerda@sansano.usm.cl>
2022-12-11 18:52:35 -0300

    admin_usuarios.js casi casi listo

--- src/Componentes/admin/admin_usuarios.js
+++ src/Componentes/admin/admin_usuarios.js
@@ -152,8 +152,8 @@ function Admin_usuarios() {
         }
     `;
     const GET_ESTADOS_CUENTAS = gql`
-        query getEstadosDeCuenta {
-            getEstadosDeCuenta {
+        query getEstadosDeCuentas {
+            getEstadosDeCuentas {
                 id
                 morosidad
                 multas 
@@ -161,7 +161,44 @@ function Admin_usuarios() {
             }
         }
     `;
+    const GET_MULTAS = gql`
+        query getMultas {
+            getMultas {
+                id
+                detalle
+                fecha
+                monto
+                pagado
+                residente
+            }
+        }
+    `;
+    const GET_GASTOS_COMUNES = gql`
+        query getGastosComunes {
+            getGastosComunes {
+                id
+                detalle
+                fecha
+                monto
+                pagado
+                residente
+            }
+        }
+    `;
+    const GET_RESERVAS = gql`
+        query getReservas {
+            getReservas {
+                id
+                fecha
+                pagado
+                residente
+                instalacion
+            }
+        }
+    `;
 
+    
+    
     //mutation's tipo DELETE
     const DELETE_RESIDENTE = gql`
         mutation Mutation($deleteResidenteId: ID!) {
@@ -200,12 +237,15 @@ function Admin_usuarios() {
     const {data: dataDirectivas, loading: loadingDirectivas, error: errorDirectivas} = useQuery(GET_DIRECTIVAS);
     const {data: dataAdministradores, loading: loadingAdministradores, error: errorAdministradores} = useQuery(GET_ADMINISTRADORES);
     const {data: dataEstadosDeCuenta, loading: loadingEstadosDeCuenta, error: errorEstadosDeCuenta} = useQuery(GET_ESTADOS_CUENTAS);
+    const {data: dataMultas, loading: loadingMultas, error: errorMultas} = useQuery(GET_MULTAS);
+    const {data: dataGastosComunes, loading: loadingGastosComunes, error: errorGastosComunes} = useQuery(GET_GASTOS_COMUNES);
+    const {data: dataReservas, loading: loadingReservas, error: errorReservas} = useQuery(GET_RESERVAS);
 
-    const {data: dataEstadoDeCuenta, loading: loadingEstadoDeCuenta, error: errorEstadoDeCuenta} = useQuery(GET_ESTADO_CUENTA,{
+/*     const {data: dataEstadoDeCuenta, loading: loadingEstadoDeCuenta, error: errorEstadoDeCuenta} = useQuery(GET_ESTADO_CUENTA,{
         variables:{
             getEstadoDeCuentaId: "63335462cedcef070b0430f6"  
         }
-    });
+    }); */
 
 /*     if (!loadingEstadoDeCuenta){
         console.log(dataEstadoDeCuenta.getEstadoDeCuenta);
@@ -318,11 +358,28 @@ function Admin_usuarios() {
           sortable: true
         },
         {
-          name: "Morosidad",
-          selector: row => row.morosidad,
-          sortable: true,
-          right: true
+          name: "Estado Cuenta",
+          selector: row => row.estadodecuenta,
+          omit: true
         },
+        {
+            name: "Morosidad",
+            selector: row => (
+                loadingEstadosDeCuenta ? "Cargando..." :
+                dataEstadosDeCuenta.getEstadosDeCuentas.map((estado) => {
+                    if (estado.residente === row.id){
+                        if (estado.morosidad === true){
+                            return "Sí"
+                        }else{
+                            return "No"
+                        }
+                    }
+                })
+                
+            ),
+            sortable: true
+        },
+
         {
           name: "Multa",
           cell: row => (
@@ -334,58 +391,83 @@ function Admin_usuarios() {
     const [filter, setFilter] = React.useState("");
     var filteredData;
     var filteredDataResi;
-    if (loadingResidentes || loadingMulta || loadingDeleteResidente || loadingAddResidente || loadingConserjes || loadingDeleteConserje || loadingDirectivas || loadingAdministradores || loadingDeleteDirectiva || loadingDeleteAdministrador || loadingAddConserje || loadingAddDirectiva || loadingAddAdministrador || loadingEstadosDeCuenta) {
+
+    if (loadingResidentes || loadingMulta || loadingDeleteResidente || loadingAddResidente || loadingConserjes || loadingDeleteConserje || loadingDirectivas || loadingAdministradores || loadingDeleteDirectiva || loadingDeleteAdministrador || loadingAddConserje || loadingAddDirectiva || loadingAddAdministrador || loadingEstadosDeCuenta || loadingMultas || loadingGastosComunes || loadingReservas) {
         filteredData = dataR
         filteredDataResi = dataResi;
     }
     else{
         //concatenar los datos de residentes y conserjes
         filteredData = dataResidentes.getResidentes.concat(dataConserjes.getConserjes, dataDirectivas.getDirectivas, dataAdministradores.getAdministradores);
-        filteredDataResi = dataResidentes.getResidentes
+        filteredDataResi = dataResidentes.getResidentes;
     }
     
-    const ExpandedComponent2 = ({dataResi}) => (
-        <div className="card" style={{width: '100%'}} >
-            <strong>Gastos Comunes:</strong> 
-            <ul>{
-                dataResi.gastos.map((gasto, index)=>(
-                    <li key={index}>{gasto}</li>
-                ))
+    const ExpandedComponent2 = ({data}) => (
+        <div className="card" style={{width: '100%'}}>
+            <strong>Gastos Comunes:</strong>
+            {
+                loadingGastosComunes ? () =>"Cargando...":
+                dataGastosComunes.getGastosComunes.length === 0 ?
+                    "No hay gastos comunes registrados" :
+                    dataGastosComunes.getGastosComunes.map((gasto) => {
+                        if (gasto.residente === data.id){
+                            return (
+                                <div className="card-body">
+                                    <p className="card-text">Monto: ${gasto.monto}</p>
+                                    <p className="card-text">Descripción: {gasto.detalle}</p>
+                                    <p className="card-text">Fecha: {gasto.fecha}</p>
+                                    <>------------------------------------------------------</>
+                                </div>
+                            )
+                        }
+                    })
+                
             }
-            </ul>
-            <strong>Multas:</strong> {dataResi.multas.length > 0 ? 
-                <ul>{
-                dataResi.multas.map((multa, index) => (
-                    <li key={index}>
-                        {multa}
-                        <button onClick={handleShow2} style={{marginLeft:'20px', border: 'none', borderRadius:'3px', padding: '0!important', fontFamily: 'arial, sans-serif,', color: '#069', cursor: 'pointer'}}> 
+            <strong>Multas:</strong>
+                <ul>
+                    <button onClick={handleShow2} style={{marginLeft:'20px', border: 'none', borderRadius:'3px', padding: '0!important', fontFamily: 'arial, sans-serif,', color: '#069', cursor: 'pointer'}}> 
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                         </svg>
-                        </button>
-                        <button onClick={() => {alert("Multa borrada")}} style={{marginLeft:'20px', border: 'none', borderRadius:'3px', padding: '0!important', fontFamily: 'arial, sans-serif,', color: '#069', cursor: 'pointer'}}> 
+                    </button>
+                    <button onClick={() => {alert("Multa borrada")}} style={{marginLeft:'20px', border: 'none', borderRadius:'3px', padding: '0!important', fontFamily: 'arial, sans-serif,', color: '#069', cursor: 'pointer'}}> 
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
-                        </button>
-                    </li>
-                    
-                ))
-                }
+                    </button>
                 </ul>
-            : "No hay multas"}
             <br></br>
             <strong>Reservas:</strong> 
-            <ul>{
-            dataResi.reservas.map((reserva, index) => (
-                <li key={index}>{reserva}</li>
-            ))
+            {
+                loadingReservas ? () =>"Cargando...":
+                dataReservas.getReservas.length === 0 ?
+                    "No hay reservas registradas" :
+                    dataReservas.getReservas.map((reserva) => {
+                        if (reserva.residente === data.id){
+                            return (
+                                <div className="card-body">
+                                    <p className="card-text">Reserva:  
+                                    {
+                                        reserva.instalacion === "6330d229c951d540fe470e4d" ?    "Estacionamiento" :
+                                        "popo"
+                                    }
+                                    </p>
+                                    <p className="card-text">Pagado: {reserva.pagado ? "Si" : "No"}
+                                    </p>
+                                    <p className="card-text">Fecha: {reserva.fecha}</p>
+                                    <>------------------------------------------------------</>
+                                </div>
+                            )
+                        }
+                    })
+                
             }
-            </ul>
         </div>
         );
     
+
+
 
     return (
         <>
